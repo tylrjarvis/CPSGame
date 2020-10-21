9c108228ba7c8cda5fb699eaf0c7bcc01fa73e89
Tanner Hermann <tannertkh@gmail.com>
2020-10-20 21:15:33 -0500

    Implemented Networking
    Networking Using Photon 2 (PUN 2 - Free).  Network.cs script controls the networking.

---
+++ CPS Game v3/Assets/NetworkController.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+*/
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      PhotonNetwork.ConnectUsingSettings(); // Connect to the photon master servers
+    }
+
+    public override void OnConnectedToMaster()
+    {
+      Debug.Log("Successfully connected to the " + PhotonNetwork.CloudRegion + " server.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CPS Game v3/Assets/Photon/NetworkController.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+*/
+
+public class NewBehaviourScript : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      PhotonNetwork.ConnectUsingSettings(); // Connect to the photon master servers
+    }
+
+    public override void OnConnectedToMaster()
+    {
+      Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
