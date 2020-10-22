d0c182c98fd5461b8f9e0f1ede3fd19b1ad272b0
Tyler Jarvis <tylrjarvis@yahoo.com>
2020-10-22 16:56:28 -0500

    Fixed menu and made it functional

---
+++ CPS Game v3/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("Level01");
+    }    public void OnClickExit()
+    {
+        print("Application will quit if on Windows or Mac");
+        Application.Quit();
+    }
+}
