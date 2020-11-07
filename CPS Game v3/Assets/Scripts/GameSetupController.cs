8595936aa9a843a0d0cfd1e05b425d887cae8efd
Tanner Hermann <tannertkh@gmail.com>
2020-11-07 15:31:21 -0600

    implemented mp

---
+++ CPS Game v3/Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.IO;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      CreatePlayer();
+        
+    }
+
+    // Update is called once per frame
+    private void CreatePlayer()
+    {
+      Debug.Log("Creating player");
+      PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+        
+    }
+}
