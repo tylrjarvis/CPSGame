29264dda30ae94b6e005a7a52421c1d7abe46234
Tyler Lucarz <taldgc@mst.edu>
2020-11-08 00:09:53 -0600

    changed UI scripts name to ClickDetection

---
+++ CPS Game v3/Assets/Scripts/ClickDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      print(gameObject.name);
+
+      gameObject.AddComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      /*
+      var colorChanger = gameObject.GetComponent<Renderer>();
+      if (Input.GetMouseButtonDown(0))
+        colorChanger.material.SetColor("_Color",Color.blue);
+
+      if (Input.GetMouseButtonDown(1))
+        colorChanger.material.SetColor("_Color",Color.green);
+
+      if (Input.GetMouseButtonDown(2))
+        colorChanger.material.SetColor("_Color",Color.yellow);
+        */
+    }
+
+    void OnMouseDown()
+    {
+      var colorChanger = gameObject.GetComponent<Renderer>();
+      colorChanger.material.SetColor("_Color",Color.green);
+    }
+}
