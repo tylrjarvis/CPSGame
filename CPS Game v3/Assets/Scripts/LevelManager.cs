1067f5f9da796e3e43e56b53b8e6907a8a6af652
Tyler Lucarz <taldgc@umsystem.edu>
2020-11-10 16:19:59 -0600

    Added Damon's bend pipe blender files. Edited the buttons to look better in the game view.

--- CPS Game v3/Assets/Scripts/LevelManager.cs
+++ CPS Game v3/Assets/Scripts/LevelManager.cs
@@ -6,18 +6,19 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void OnClickStart()
     {
         SceneManager.LoadScene("Level01");
-    }    public void OnClickExit()
+    }
+    public void OnClickExit()
     {
         print("Application will quit if on Windows or Mac");
         Application.Quit();
