a16bd1e3d221d1e0bc6fa669abe5384a71d15015
Tyler Jarvis <tylrjarvis@yahoo.com>
2020-12-09 15:22:34 -0600

    Buildable

--- CPS Game v3/Assets/Scripts/GameVariables.cs
+++ CPS Game v3/Assets/Scripts/GameVariables.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms;
